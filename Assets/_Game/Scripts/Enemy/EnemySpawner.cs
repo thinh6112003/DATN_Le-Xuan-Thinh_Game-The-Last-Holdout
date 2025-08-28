f4d1ce415c78c82d138275aa40f8df6a1e13f40f
Lê Xuân Thịnh <lexuanthinh01@gmail.com>
2025-08-29 01:43:22 +0700

    [Feature] thêm logic random cho Enemy Spawner

--- Assets/_Game/Scripts/Enemy/EnemySpawner.cs
+++ Assets/_Game/Scripts/Enemy/EnemySpawner.cs
@@ -8,6 +8,7 @@ public class EnemySpawner : MonoBehaviour
     public BaseEnemy baseEnemyPrefab;
     public BaseEnemy baseEnemyPrefab2;
     public List<Transform> listWaypoint;
+    public List<Transform> listWaypoint2;
     public float timeSpawnNewEnemy;
     void Start()
     {
@@ -18,9 +19,12 @@ public class EnemySpawner : MonoBehaviour
         while (true) 
         {
             int random = Random.Range(0,2);
+            int random2 = Random.Range(0,2);
             BaseEnemy newEnemy = Instantiate( random==0 ? baseEnemyPrefab : baseEnemyPrefab2);
+            newEnemy.gameObject.SetActive(true);
             newEnemy.transform.position = spawnPos.position;
-            newEnemy.waypointMover.waypoints = listWaypoint;
+            newEnemy.waypointMover.waypoints = random2 == 0 ? listWaypoint: listWaypoint2;
+
             float randomTime = Random.Range(timeSpawnNewEnemy, timeSpawnNewEnemy * 1.75f);
             yield return new WaitForSeconds(timeSpawnNewEnemy);
         }
